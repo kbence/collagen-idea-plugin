fd42809cfb3ee8f57d55cc0d877ccc9b0b096dc1
Bence Kiglics <bence.kiglics@gmail.com>
2014-05-06 16:40:49 +0200

    added a few controls

--- src/main/clojure/org/collagen/plugin/CollagenPlugin.clj
+++
@@ -1,7 +0,0 @@
-(ns org.collagen.plugin.CollagenPlugin
-  (:gen-class
-    :name org.collagen.plugin.CollagenPlugin
-    :prefix plugin-))
-
-(defn plugin-main []
-  (println "Collagen"))
--- src/main/clojure/org/collagen/plugin/CollagenToolWindowFactory.clj
+++ src/main/clojure/org/collagen/plugin/CollagenToolWindowFactory.clj
@@ -1,7 +1,8 @@
 (ns org.collagen.plugin.CollagenToolWindowFactory
   (:import [com.intellij.openapi.wm ToolWindowFactory ToolWindow]
     [com.intellij.openapi.project Project]
-    [javax.swing JLabel])
+    [javax.swing JLabel JPanel JButton JTextField BoxLayout Box JTextArea]
+    [java.awt FlowLayout])
   (:gen-class
     :name org.collagen.plugin.CollagenToolWindowFactory
     :implements [com.intellij.openapi.wm.ToolWindowFactory]))
@@ -9,6 +10,21 @@
 (defn contentFactory []
   (com.intellij.ui.content.ContentFactory$SERVICE/getInstance))
 
+(defn toolWindowContent []
+  (let [rootPanel (JPanel.)
+        rootLayout (BoxLayout. rootPanel BoxLayout/PAGE_AXIS)
+        serverPanel (JPanel. (FlowLayout. FlowLayout/LEFT 5 5))
+        clientPanel (JPanel. (FlowLayout. FlowLayout/LEFT 5 5))]
+    (.add serverPanel (JButton. "Start server"))
+    (.add clientPanel (JLabel. "Host:"))
+    (.add clientPanel (JTextField. 20))
+    (.add clientPanel (JButton. "Connect"))
+    (.setLayout rootPanel rootLayout)
+    (.add rootPanel serverPanel)
+    (.add rootPanel clientPanel)
+    (.add rootPanel (JToolBar.))
+    rootPanel))
+
 (defn -createToolWindowContent [this #^Project project #^ToolWindow toolWindow]
   (.addContent (.getContentManager toolWindow)
-    (.createContent (contentFactory) (JLabel. "Collagen plugin") "" false)))
+    (.createContent (contentFactory) (toolWindowContent) "" false)))
